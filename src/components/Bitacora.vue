09982d723282c3ef95279b4ae3de0c58f8943862
JulianJJ5 <juliandj96@gmail.com>
2024-10-03 17:35:21 -0500

    Initial commit

---
+++ src/components/Bitacora.vue
@@ -0,0 +1,356 @@
+<template>
+  <div class="q-pa-md">
+    <q-layout view="lHh Lpr lff">
+      <p><b>Fecha Actual:</b> {{ dia }} de {{ mesNombre }} del {{ anio }}</p>
+      <div class="botones">
+        <q-btn
+          :disable="loading"
+          @click="dialogo('crear')"
+          label="Crear Bitacora"
+          class="crear colorCorporativo"
+        />
+        <q-btn :disable="loading" class="crearPDF colorCorporativo" to="/Tabla">
+          <font-awesome-icon icon="file-invoice" style="color: #ffffff" />
+        </q-btn>
+      </div>
+      <br />
+      <q-dialog v-model="prompt" persistent :style="{ zIndex: 1000 }">
+        <q-card style="min-width: 350px">
+          <q-card-section>
+            <div class="text-h5 tituloCuadroDeDialogo">{{ dialogTitle }}</div>
+          </q-card-section>
+
+          <q-select
+            filled
+            clearable
+            v-model="selectedAprendiz"
+            :options="aprendices"
+            use-input
+            option-label="documento"
+            option-value="_id"
+            label="Seleccionar Aprendiz"
+            autofocus
+            :disable="loading"
+            
+          >
+          
+            <template v-slot:prepend>
+              <font-awesome-icon icon="user-graduate" />
+            </template>
+          </q-select>
+
+          <q-input
+            filled
+            v-model="fecha"
+            label="Seleccionar fecha"
+            type="date"
+            :disable="loading"
+          >
+            <template v-slot:prepend>
+              <font-awesome-icon icon="spell-check" />
+            </template>
+          </q-input>
+
+          <q-card-actions align="right" class="text-primary1">
+            <q-btn flat v-close-popup class="btnCerrar" :disable="loading">
+              <font-awesome-icon
+                icon="fa-solid fa-circle-xmark"
+                style="margin-right: 5px"
+              />
+              Cerrar
+            </q-btn>
+            <q-btn
+              flat
+              class="btnGuardar"
+              @click="validar"
+              :loading="loading"
+              :disable="loading"
+            >
+              <font-awesome-icon
+                icon="fa-solid fa-floppy-disk"
+                style="margin-right: 5px"
+              />
+              Guardar Bitacora
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+
+      <q-table
+        title="BITÁCORAS"
+        :rows="rows"
+        :columns="columns"
+        row-key="name"
+        pagination.sync="pagination"
+        :rows-per-page-options="[20, 50, 100, 0]"
+        :loading="loading"
+      >
+        <div>
+          <hr style="border: 1px solid black; width: 50%; margin: auto" />
+        </div>
+        <template v-slot:body-cell-opciones="props">
+          <q-td :props="props">
+            <q-form>
+              <q-select
+                v-model="props.row.estado"
+                :options="estados"
+                label="Estado"
+                filled
+                :disable="loading"
+                @update:model-value="
+                  (valor) => cambiarEstado(props.row._id, valor)
+                "
+              />
+            </q-form>
+          </q-td>
+        </template>
+      </q-table>
+    </q-layout>
+  </div>
+</template>
+
+<script setup>
+import { ref, onBeforeMount } from "vue";
+import { useQuasar } from "quasar";
+import { useBitacoraStore } from "../stores/bitacora.js";
+import { useAprendizStore } from "../stores/aprendiz.js";
+import { useFichaStore } from "../stores/Ficha.js"
+
+onBeforeMount(() => {
+  traer();
+  cargarAprendices();
+  obtenerFechaActual();
+});
+
+let dia = ref(null);
+let mesNumero = ref(null);
+let mesNombre = ref(null);
+let anio = ref(null);
+
+const meses = ref([
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre,",
+  "Noviembre",
+  "Diciembre",
+]);
+const prompt = ref(false);
+const loading = ref(false);
+const selectedAprendiz = ref(null);
+const fecha = ref(null);
+const aprendices = ref([]);
+const dialogTitle = ref("");
+const q$ = useQuasar();
+const estados = ref(["Asistió", "No Asistió", "Pendiente", "Excusa"]);
+const useBitacora = useBitacoraStore();
+const useAprendiz = useAprendizStore();
+const rows = ref([]);
+const columns = ref([
+  {
+    name: "cedulaAprendiz",
+    required: true,
+    label: "Cédula del Aprendiz",
+    align: "center",
+    field: "cedulaAprendiz",
+    sortable: true,
+  },
+  {
+    name: "nombre",
+    required: true,
+    label: "Nombre del Aprendiz",
+    align: "center",
+    field: "nombre",
+    sortable: true,
+  },
+  {
+    name: "fecha1",
+    align: "center",
+    label: "Fecha",
+    field: "fecha",
+    sortable: true,
+  },
+  {
+    name: "opciones",
+    label: "Estado",
+    align: "center",
+    sortable: true,
+  },
+]);
+
+async function traer() {
+  loading.value = true;
+  try {
+    let response = await useBitacora.listarTodo();
+    let response2 = await useAprendiz.listarAprendiz();
+
+    rows.value = response.data.bitacoras.map((bitacora) => {
+      let aprendiz = response2.data.find(
+        (aprendiz) => aprendiz?._id === bitacora?.id_aprendiz
+      );
+      return {
+        ...bitacora,
+        cedulaAprendiz: aprendiz?.documento || "No disponible",
+        nombre: aprendiz?.nombre || "Desconocido",
+        fecha: formatFecha(bitacora?.fecha || new Date()),
+      };
+    });
+  } finally {
+    loading.value = false;
+  }
+}
+
+function obtenerFechaActual() {
+  const fecha = new Date();
+
+  dia = fecha.getDate();
+  mesNumero = fecha.getMonth();
+  mesNombre.value = meses.value[fecha.getMonth()];
+  anio = fecha.getFullYear();
+
+  console.log(fecha);
+
+  return {
+    día: dia,
+    mesNumero: mesNumero,
+    mesNombre: mesNombre,
+    año: anio,
+  };
+}
+
+async function cargarAprendices() {
+  loading.value = true;
+  try {
+    const response = await useAprendiz.listarAprendiz();
+    
+    aprendices.value = response.data.map((aprendiz) => ({
+      _id: aprendiz._id,
+      documento: `${aprendiz.documento} - ${aprendiz.nombre}`,
+    }));
+  } catch (error) {
+    q$.notify({
+      type: "negative",
+      message: "Error al cargar los aprendices.",
+    });
+    console.error("Error al cargar los aprendices:", error);
+  } finally {
+    loading.value = false;
+  }
+}
+
+const cambiarEstado = async (id, estado) => {
+  try {
+    loading.value = true;
+    await useBitacora.actualizarEstado(id, estado);
+    await traer();
+  } catch (error) {
+    console.error("Error al cambiar estado:", error);
+  } finally {
+    loading.value = false;
+  }
+};
+
+function formatFecha(fecha) {
+  const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+  return new Date(fecha).toLocaleDateString("es-ES", options);
+}
+
+const dialogo = (accion, bitacora = null) => {
+  if (accion === "crear") {
+    dialogTitle.value = "Crear Bitácora";
+    selectedAprendiz.value = null;
+    fecha.value = null;
+  } else if (accion === "editar" && bitacora) {
+    dialogTitle.value = "Editar Bitácora";
+    selectedAprendiz.value =
+      aprendices.value.find(
+        (aprendiz) => aprendiz._id === bitacora.aprendizId
+      ) || null;
+    fecha.value = bitacora.fecha || null;
+  }
+  prompt.value = true;
+};
+
+const validar = async () => {
+  if (!selectedAprendiz.value || !fecha.value) {
+    q$.notify({
+      type: "negative",
+      message: "Rellena todos los campos.",
+    });
+    return;
+  }
+
+  loading.value = true;
+  try {
+    const bitacoraData = {
+      id_aprendiz: selectedAprendiz.value,
+      fecha: fecha.value,
+    };
+
+    if (dialogTitle.value === "Editar Bitácora") {
+      await useBitacora.actualizar(bitacoraData);
+    } else {
+      await useBitacora.crearBitacora(bitacoraData);
+    }
+
+    await traer();
+    prompt.value = false;
+  } catch (error) {
+    q$.notify({
+      type: "negative",
+      message: "Error al guardar la bitácora.",
+    });
+    console.error(
+      "Error al guardar la bitacora:",
+      error.response ? error.response.data : error.message
+    );
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<style>
+.botones {
+  display: flex !important;
+  justify-content: space-between !important;
+  align-items: end !important;
+}
+
+.q-table__container div {
+  display: flex;
+  justify-content: center;
+}
+
+.q-table__container > div:first-child {
+  border-bottom: rgb(0, 136, 11) 4px solid;
+  padding: 2%;
+}
+
+.q-table--horizontal-separator thead th,
+.q-table--horizontal-separator tbody tr:not(:last-child) > td,
+.q-table--cell-separator thead th,
+.q-table--cell-separator tbody tr:not(:last-child) > td {
+  border-bottom-width: 3px !important;
+}
+
+.q-table__top {
+  padding: 3px !important;
+}
+
+th.text-center {
+  font-size: 15px !important;
+  font-weight: bold !important;
+  color: rgb(1, 87, 0);
+}
+
+td {
+  padding: 2% !important;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,275 @@
+<template>
+  <div class="q-pa-md">
+    <q-layout view="lHh Lpr lff">
+      <q-header elevated class="colorCorporativo text-white">
+        <q-toolbar>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+          <q-toolbar-title>REPFORA</q-toolbar-title>
+          <img
+            src="../assets/Images/Logo-Back-Home.png"
+            @click="goToRoute('/Home')"
+            class="width"
+          />
+        </q-toolbar>
+      </q-header>
+
+      <div v-if="isLoading" class="fullscreen-spinner">
+        <q-spinner color="primary" size="3em" :thickness="2" />
+      </div>
+
+      <div v-else>
+        <q-drawer v-model="drawer" show-if-above :width="220" :mini-to-overlay="false" :breakpoint="400">
+          <q-scroll-area
+            style="
+              height: calc(100% - 150px);
+              margin-top: 150px;
+              border-right: 1px solid #ddd;
+            "
+          >
+            <q-list padding>
+              <q-btn class="menuButton colorCorporativo" to="/Home" label="HOME" />
+              <br/><br/>
+              <q-btn class="menuButton colorCorporativo" to="/Home/Usuario" label="ADMINISTRADORES" />
+              <br/><br/>
+              <q-btn class="menuButton colorCorporativo" to="/Home/Aprendiz" label="APRENDICES" />
+              <br/><br/>
+              <q-btn class="menuButton colorCorporativo" to="/Home/Ficha" label="FICHAS" />
+              <br/><br/>
+              <q-btn class="menuButton colorCorporativo" to="/Home/Bitacora" label="BITACORAS" />
+              <br/><br/>
+              <q-btn class="menuButton colorCorporativo" to="/Home/Informes" label="INFORMES" />
+              <q-btn class="menuButtonSession" to="/" label="Cerrar sesión" @click="logout()"/>
+
+            </q-list>
+          </q-scroll-area>
+
+          <q-img
+            class="absolute-top"
+            src="../assets/Images/fondo_q-drawer.avif"
+            style="height: 150px"
+          >
+            <div class="absolute-bottom bg-transparent">
+              <q-avatar size="56px" class="q-mb-sm">
+                <img src="../assets/Images/logo-blanco.png" />
+              </q-avatar>
+              <div class="text-weight-bold">{{ nombreUsuario }}</div>
+              <div class="text-weight-bold">{{ emailUsuario }}</div>
+            </div>
+          </q-img>
+        </q-drawer>
+
+        <!-- Ajustamos la clase q-layout-padding para que el contenido se mueva -->
+        <q-page-container>
+          <router-view></router-view>
+
+          <div class="q-gutter-md grid" v-if="$route.path === '/Home'">
+            <q-card class="my-card" flat bordered @click="goToRoute('/Home/Ficha')">
+              <div class="colorCorporativo flex-container">
+                <q-img src="../assets/Images/Logo-Fichas.png" class="flex-img" />
+              </div>
+              <q-card-section class="flex-content">
+                <div class="text-h6 q-mt-sm q-mb-xs">FICHAS</div>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="my-card" flat bordered @click="goToRoute('/Home/Usuario')">
+              <div class="colorCorporativo flex-container">
+                <q-img src="../assets/Images/Logo-Administrador.png" class="flex-img" />
+              </div>
+              <q-card-section class="flex-content">
+                <div class="text-h6 q-mt-sm q-mb-xs">ADMINISTRADOR</div>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="my-card" flat bordered @click="goToRoute('/Home/Bitacora')">
+              <div class="colorCorporativo flex-container">
+                <q-img src="../assets/Images/Logo-Bitacoras.png" class="flex-img" />
+              </div>
+              <q-card-section class="flex-content">
+                <div class="text-h6 q-mt-sm q-mb-xs">BITACORAS</div>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="my-card" flat bordered @click="goToRoute('/Home/Aprendiz')">
+              <div class="colorCorporativo flex-container">
+                <q-img src="../assets/Images/Logo-Aprendices.png" class="flex-img" />
+              </div>
+              <q-card-section class="flex-content">
+                <div class="text-h6 q-mt-sm q-mb-xs">APRENDICES</div>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="my-card" flat bordered @click="goToRoute('/Home/Informes')">
+              <div class="colorCorporativo flex-container">
+                <q-img src="../assets/Images/Logo-Informes.png" class="flex-img" />
+              </div>
+              <q-card-section class="flex-content">
+                <div class="text-h6 q-mt-sm q-mb-xs">INFORMES</div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </q-page-container>
+      </div>
+    </q-layout>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { useUsuarioStore } from "../stores/usuarios";
+
+const drawer = ref(false);
+const router = useRouter();
+const isLoading = ref(false)
+const nombreUsuario = ref('')
+const emailUsuario = ref('')
+const useUsuario = useUsuarioStore()
+
+
+function goToRoute(route) {
+  router.push(route);
+}
+
+onMounted(() => {
+    nombreUsuario.value = sessionStorage.getItem('nombre') || ''
+    emailUsuario.value = sessionStorage.getItem('email') || ''
+  });
+
+  const logout = async () => {
+    const r  = useUsuario.logout()
+  }
+</script>
+
+<style>
+.grid {
+  display: grid !important;
+  grid-template-columns: repeat(3, 1fr) !important;
+}
+
+.q-gutter-md {
+  padding-inline-start: 10% !important;
+}
+
+.absolute-top{
+  text-align: center;
+}
+
+.my-card {
+  width: 65%;
+  cursor: pointer;
+  border-radius: 0px !important;
+  border: 5px #2e7d32 solid !important;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+
+.my-card:hover {
+  transform: translateY(-10px);
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.width {
+  width: 50%;
+}
+
+.text-h5 {
+  text-align: center;
+}
+
+.menuButton {
+  color: white !important;
+  font-size: 17px !important;
+  width: 90% !important;
+  margin-left: 4% !important;
+}
+
+.menuButtonSession {
+  background-color: #df1f1f !important;
+  color: white !important;
+  font-size: 17px !important;
+  width: 90% !important;
+  margin-left: 4% !important;
+  top: 50px !important;
+  position: relative !important;
+}
+
+.q-table__title{
+  font-size: 30px !important;
+  font-weight: bold !important;
+}
+
+.btnCerrar {
+  background-color: #ff0404cd !important;
+  font-weight: bold !important;
+  color: white !important;
+}
+
+.btnGuardar {
+  background-color: #2e7d32 !important;
+  color: white !important;
+  font-weight: bold !important;
+}
+
+.q-card .tituloCuadroDeDialogo {
+  background-color: #2e7d32 ;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color: white;
+  padding: 17px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.q-select{
+  padding: 10px;
+
+}
+
+.q-input {
+padding: 10px;
+}
+
+.q-file {
+  padding: 10px;
+}
+
+.colorCorporativo{
+  background-color: #238327 !important;
+  color: white !important;
+}
+
+.desactivar {
+  background-color: red !important;
+  border-radius: 13px !important;
+  margin-left: 2% !important;
+}
+
+.activar{
+  background-color: rgb(10, 190, 10) !important;
+  border-radius: 13px !important;
+  margin-left: 2% !important;
+}
+
+.editar{
+  border-radius: 13px !important;
+  margin-right: 2% !important;
+}
+
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+}
+
+.flex-img {
+  max-width: 80%;
+  height: auto;
+}
+
+.flex-content {
+  text-align: center;
+  margin-top: 8px; 
+}
+
+</style>
